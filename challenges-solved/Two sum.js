efe0f2f7d084ffc8e9c533350ee16f1227acfa17
TechRami <techrami@outlook.com>
2023-04-27 19:21:18 +0300

    solved a new question

---
+++ challenges-solved/Two sum.js
@@ -0,0 +1,10 @@
+function twoSum(numbers, target) {
+    const map = new Map()
+    for(let i = 0; i < numbers.length; i++) {
+      const complement = target - numbers[i]
+      if(map.has(complement)) {
+        return [map.get(complement), i]
+      }
+      map.set(numbers[i], i)
+    }
+  }

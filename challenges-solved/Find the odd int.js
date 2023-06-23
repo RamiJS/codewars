0e0c353fb6f75206eb46f8359a7bc60b4435f1e0
TechRami <techrami@outlook.com>
2023-06-23 11:32:00 +0300

    ez

---
+++ challenges-solved/Find the odd int.js
@@ -0,0 +1,19 @@
+function findOdd(A) {
+    let result = 0
+    let map = new Map()
+    
+    for(let i = 0; i < A.length; i++) {
+        if(map.has(A[i])) {
+            map.set(A[i], map.get(A[i]) + 1) 
+        } else {
+            map.set(A[i], 1)
+        }
+    }
+    
+    map.forEach((i, key) => (
+        i % 2 === 0 ? 0 : result = key
+    ))
+    return result;
+}
+  
+console.log(findOdd([0,1,0,1,0]))
